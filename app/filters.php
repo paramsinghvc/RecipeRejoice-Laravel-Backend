9ff40814fd77d81058478b6b53c8a6e445a42bde
Parminder Singh <paramsinghvc@gmail.com>
2015-04-26 21:37:52 +0530

    filters cors

--- app/filters.php
+++ app/filters.php
@@ -9,16 +9,16 @@
 | which may be used to do any work before or after a request into your
 | application. Here you may also register your custom route filters.
 |
-*/
+ */
 
-App::before(function($request)
-{
-	//
+App::before(function ($request) {
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+	header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+	header('Access-Control-Allow-Credentials: true');
 });
 
-
-App::after(function($request, $response)
-{
+App::after(function ($request, $response) {
 	//
 });
 
@@ -31,26 +31,19 @@ App::after(function($request, $response)
 | session is logged into this application. The "basic" filter easily
 | integrates HTTP Basic authentication for quick, simple checking.
 |
-*/
+ */
 
-Route::filter('auth', function()
-{
-	if (Auth::guest())
-	{
-		if (Request::ajax())
-		{
+Route::filter('auth', function () {
+	if (Auth::guest()) {
+		if (Request::ajax()) {
 			return Response::make('Unauthorized', 401);
-		}
-		else
-		{
+		} else {
 			return Redirect::guest('login');
 		}
 	}
 });
 
-
-Route::filter('auth.basic', function()
-{
+Route::filter('auth.basic', function () {
 	return Auth::basic();
 });
 
@@ -63,11 +56,13 @@ Route::filter('auth.basic', function()
 | it simply checks that the current user is not logged in. A redirect
 | response will be issued if they are, which you may freely change.
 |
-*/
+ */
 
-Route::filter('guest', function()
-{
-	if (Auth::check()) return Redirect::to('/');
+Route::filter('guest', function () {
+	if (Auth::check()) {
+		return Redirect::to('/');
+	}
+
 });
 
 /*
@@ -79,12 +74,10 @@ Route::filter('guest', function()
 | cross-site request forgery attacks. If this special token in a user
 | session does not match the one given in this request, we'll bail.
 |
-*/
+ */
 
-Route::filter('csrf', function()
-{
-	if (Session::token() !== Input::get('_token'))
-	{
+Route::filter('csrf', function () {
+	if (Session::token() !== Input::get('_token')) {
 		throw new Illuminate\Session\TokenMismatchException;
 	}
 });
