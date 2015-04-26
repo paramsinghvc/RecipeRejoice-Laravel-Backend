22c4ad00b39816efe2cc057d3da1644f8a4ad709
Parminder Singh <paramsinghvc@gmail.com>
2015-04-26 21:50:16 +0530

    a

--- app/filters.php
+++ app/filters.php
@@ -13,9 +13,9 @@
 
 App::before(function ($request) {
 	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 	header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-	header('Access-Control-Allow-Credentials: true');
+
 });
 
 App::after(function ($request, $response) {
